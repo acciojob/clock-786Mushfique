574dfd2f6a980c0aa1fd8bd4e0dd6e7f82091a2b
Md Mushfique Raza <114329904+786Mushfique@users.noreply.github.com>
2026-01-13 15:42:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your JS code here. If required.
+function updateTimer() {
+    const now = new Date();
+    const formatted = now.toLocaleString("en-US", {
+      month: "numeric",
+      day: "numeric",
+      year: "numeric",
+      hour: "numeric",
+      minute: "2-digit",
+      second: "2-digit",
+      hour12: true
+    });
+
+    document.getElementById("timer").textContent = formatted;
+    }
+    updateTimer();
+    setInterval(updateTimer, 1000);
